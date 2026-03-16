af4e6cc0f7289199ac33cdcc47446b3c0565c596
zukkc <zukkfp@gmail.com>
2026-03-16 14:29:09 +0100

    feat: main page layout with some static data

---
+++ js/setupDailyWeatherChart.js
@@ -0,0 +1,32 @@
+  const ctx = document.getElementById('daily-weather-chart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: [
+        '00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00','08:00', '09:00', '10:00', 
+        '11:00','12:00', '13:00', '14:00', '15:00','16:00', '17:00', '18:00', '19:00','20:00', 
+        '21:00', '22:00','23:00'
+      ],
+      datasets: [
+      {
+        label: 'temperatura (°C)',
+        data: [12, 19, 3, 5, 2, 3, 5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6],
+        type: 'line',
+        borderWidth: 1
+      },
+      {
+        label: 'Szansa na opady (%)',
+        data: [4, 5, 3, 5, 56, 3, 5,59,36,5,5,12,5,0,5,0,0,5,0,5,20,5,100],
+        borderWidth: 1
+      }
+      ]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
